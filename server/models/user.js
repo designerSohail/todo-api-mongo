68724f281424bd80b0ba1c571bf23e2f13dcab4e
designerSohail <designersohail786@gmail.com>
2018-02-06 11:05:44 +0545

    POSt /todos route added and refactored server folder

---
+++ server/models/user.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+var User = mongoose.model('User', {
+	name: {
+		type: String,
+		required: true,
+		minlength: 1,
+		trim: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		minlength: 1,
+		trim: true
+	}
+
+})
+module.exports = {User}
---
+++ server/server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const {mongoose} = require('./db/mongoose')
+const {User} = require('./models/user')
+const {Todo} = require('./models/todo')
+
+const app = express()
+
+app.use(bodyParser.json())
+
+app.post('/todos', (req, res) => {
+	var todo = new Todo({
+		name: req.body.name
+	})
+	todo.save().then(doc => {
+		log('Successfully saved todo: ', doc.name)
+		res.send(doc)
+	}, err => {
+		log('Unable to save document ', err)
+		res.status(400).send(err)
+	})
+})
+
+app.listen(3000, () => {
+	log('Server is running.')
+})
+function log(str, arg = null) {
+	console.log(str, arg)
+}
