15f3af456d6bb078b0b7a7c642071106762ddfa4
designerSohail <designersohail786@gmail.com>
2018-02-08 11:08:12 +0545

    Host fixed

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,4 +1,4 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://developerk:myDatabase786@ds225028.mlab.com:25028/todo-app' || 'mongodb://localhost:27017/TodoApp')
+mongoose.connect('mongodb://developerk:myDatabase786@ds225028.mlab.com:25028/todo-app')
 module.exports.mongoose = mongoose
