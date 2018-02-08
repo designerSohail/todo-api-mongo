9adbaf76950c7e26120c87eb4f3d5af14d988eac
designerSohail <designersohail786@gmail.com>
2018-02-08 11:28:30 +0545

    Bugs fixed

--- server/server.js
+++ server/server.js
@@ -34,7 +34,7 @@ app.get('/todos', (req, res) => {
 app.get('/todos/:id', (req, res) => {
 	Todo.findById().then(todo => {
 		const id = req.params.id
-		// if (!ObjectID.isValid(id)) return res.status(404).send('Requested todo id is not in valid format!')
+		if (!ObjectID.isValid(id)) return res.status(404).send('Requested todo id is not in valid format!')
 		Todo.findById(id).then(todo => {
 			if (!todo) return res.send(404).send('Requested todo not found!')
 			res.send({todo})
